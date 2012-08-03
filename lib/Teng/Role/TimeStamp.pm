d7e56fee067b78831a4cac94cc7cfcd9e3b2dc87
Shinichiro Sei <sei@kayac.com>
2012-08-03 15:02:48 +0900

    initial commit

---
+++ lib/Teng/Role/TimeStamp.pm
@@ -0,0 +1,32 @@
+package Teng::Role::TimeStamp;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Teng::Role::TimeStamp -
+
+=head1 SYNOPSIS
+
+  use Teng::Role::TimeStamp;
+
+=head1 DESCRIPTION
+
+Teng::Role::TimeStamp is
+
+=head1 AUTHOR
+
+Shinichiro Sei E<lt>shin1rosei@kayac.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
